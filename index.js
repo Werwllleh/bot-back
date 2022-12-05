7e8f37043f523d773740d8d4758688c732745030
Werwllleh <allvxm4@gmail.com>
2022-12-05 16:04:21 +0300

    1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 app.use(fileUpload({}));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
 app.listen(port, () =>
 	console.log(`App is listening on port ${port}.`)
